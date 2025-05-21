8e445640a4934c9820bee902ae02345c234ee648
rukhov <ukhov.roman@gmail.com>
2025-05-21 19:23:31 +0300

    -

--- cmake/FindLibAtvTools.cmake
+++ cmake/FindLibAtvTools.cmake
@@ -1,3 +1,3 @@
 include(FetchContent)
 
-find_package(lib-atv-tools REQUIRED PATHS /home/rukhov/projects/analog-tv-tools/install-lib-atv-tools/cmake/lib-atv-tools)
+find_package(lib-atv-tools REQUIRED PATHS ~/projects/analog-tv-tools/install-lib-atv-tools/cmake/lib-atv-tools)
--- cmake/FindRTSPServerTemplate.cmake
+++ cmake/FindRTSPServerTemplate.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
   rtsp_server_template 
   GIT_REPOSITORY https://github.com/rukhov/rtsp_server_template.git
-  GIT_TAG        origin/Improve-quality
+  GIT_TAG        origin/main
 )
 set(BUILD_EXAMPLES OFF)
 FetchContent_MakeAvailable( rtsp_server_template )
